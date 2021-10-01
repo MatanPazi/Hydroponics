c8ad79dad25d15cc7f4c60d273f5eb4ccdf13d62
MatanPazi <74129593+MatanPazi@users.noreply.github.com>
2021-10-01 15:08:21 +0300

    Update Hydroponics.ino

--- Hydroponics.ino
+++ Hydroponics.ino
@@ -1,9 +1,65 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ * DHT part credited to http://www.esp32learning.com/code/esp32-and-am2301-sensor-example.php
+ * Connecting to ESP32:
+ * https://randomnerdtutorials.com/installing-the-esp32-board-in-arduino-ide-windows-instructions/
+ * Analog inputs:
+ * https://randomnerdtutorials.com/esp32-adc-analog-read-arduino-ide/
+ */
+#include "DHT.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+//
+#define DHTPIN 12                     //Modify to the pin we're connected to (IO 12). 
+#define DHTTYPE DHT21                 //AM2301 
+#define ONE_WIRE_BUS 12               //IO12
+//
+const uint16_t pHPin = 34;            //pH analog input is connected to GPIO 34 (Analog ADC1_CH6)
+const uint16_t TDSPin = 35;           //TDS analog input is connected to GPIO 35 (Analog ADC1_CH7)
+uint16_t pHVal = 0
+uint16_t TDSVal = 0
 
+DHT dht(DHTPIN, DHTTYPE);
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+ 
+void setup() 
+{
+  //ADC default attenuation is 11db , so no need to set it (~2600 mV FS).
+  Serial.begin(9600); 
+  sensors.begin();
+  delay(200);
+  if (sensors.getDS18Count() != 0) sensors.setResolution(12););
+  dht.begin();
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+ 
+void loop() 
+{
+  uint8_t Water_Temp = 0;
+  pHVal = analogRead(pHPin);
+  TDSVal = analogRead(TDSPin);
+  if (sensors.getDS18Count() != 0) 
+  {
+    sensors.requestTemperatures();
+    Water_Temp = sensors.getTempCByIndex(0); 
+  } 
+  float Humidity = dht.readHumidity();
+  float Amb_Temp = dht.readTemperature();
+  if (isnan(Amb_Temp) || isnan(Humidity) || Water_Temp == 0)   //Check if returns are valid, if they are NaN (not a number) then something went wrong!
+  {
+    Serial.println("Failed to read from AM2301 or DS1820");
+  } 
+  else
+  {
+    Serial.print("Humidity: "); 
+    Serial.print(Humidity);
+    Serial.print(" %\t");
+    Serial.print("Ambient Temperature: "); 
+    Serial.print(Amb_Temp);
+    Serial.print(" C");
+    Serial.print("\t");
+    Serial.print("Water Temperature");
+    Serial.print(Water_Temp);
+    Serial.println(" C");
+    delay(5000);
+ }
 }
