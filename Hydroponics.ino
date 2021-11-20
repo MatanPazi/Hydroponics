422f03298a32b7c47d65ac9adc566c1d968e3a6a
MatanPazi <74129593+MatanPazi@users.noreply.github.com>
2021-11-20 14:00:28 +0200

    Update Hydroponics.ino

--- Hydroponics.ino
+++ Hydroponics.ino
@@ -1,74 +1,86 @@
-/*
- * DHT part credited to http://www.esp32learning.com/code/esp32-and-am2301-sensor-example.php
- * Connecting to ESP32:
- * https://randomnerdtutorials.com/installing-the-esp32-board-in-arduino-ide-windows-instructions/
- * Analog inputs:
- * https://randomnerdtutorials.com/esp32-adc-analog-read-arduino-ide/
- * I2C LCD Display:
- * https://www.circuitschools.com/interfacing-16x2-lcd-module-with-esp32-with-and-without-i2c/#Method_2_Interfacing_16X2_LCD_module_with_ESP_32_using_I2C_adapter
- */
+// Example testing sketch for various DHT humidity/temperature sensors written by ladyada
+// REQUIRES the following Arduino libraries:
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
 
 #include "DHT.h"
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <LiquidCrystal_I2C.h>
-//
-#define DHTPIN 12                     //Modify to the pin we're connected to (IO 12). 
-#define DHTTYPE DHT21                 //AM2301 
-#define ONE_WIRE_BUS 12               //IO12
-//
+
+//Ambient temp and humidity sensor (AM2301)
+#define DHTPIN 4     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+//Water temp sensor (ds18b20)
+#define ONE_WIRE_BUS 11               //IO11
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
 const uint16_t pHPin = 34;            //pH analog input is connected to GPIO 34 (Analog ADC1_CH6)
 const uint16_t TDSPin = 35;           //TDS analog input is connected to GPIO 35 (Analog ADC1_CH7)
-uint16_t pHVal = 0
-uint16_t TDSVal = 0
-
+uint16_t pHVal = 0;
+uint16_t TDSVal = 0;
+//y = ax + b, where x are the pH sensor readings and y is the actual pH value
+//measured using water (7 pH) and lemon extract (2 pH)
+const float a = -10.0;
+const float b = 22.5;
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
 DHT dht(DHTPIN, DHTTYPE);
+
 OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
-LiquidCrystal_I2C LCD(0x27, 16,2);  //Will need to scan for I2C addresses if 0x27 isn't correct
- 
-void setup() 
-{
-  //ADC default attenuation is 11db , so no need to set it (~2600 mV FS).
+DallasTemperature ds18b20(&oneWire);
+LiquidCrystal_I2C LCD(0x3F, 16,2);  //
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+  delay(100);
   LCD.init();
   LCD.backlight();
-  Serial.begin(9600); 
-  sensors.begin();
-  delay(200);
-  if (sensors.getDS18Count() != 0) sensors.setResolution(12););
-  dht.begin();
+  delay(100);  
+  ds18b20.begin();
+  delay(100);
+  if (ds18b20.getDS18Count() != 0) ds18b20.setResolution(12);
+  delay(100);
+  dht.begin();
 }
- 
-void loop() 
-{
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
   LCD.setCursor(0, 1);
   uint8_t Water_Temp = 0;
-  pHVal = analogRead(pHPin);
+  pHVal = a*analogRead(pHPin) + b;
   TDSVal = analogRead(TDSPin);
-  if (sensors.getDS18Count() != 0) 
+  if (ds18b20.getDS18Count() != 0) 
   {
-    sensors.requestTemperatures();
-    Water_Temp = sensors.getTempCByIndex(0); 
+    ds18b20.requestTemperatures();
+    Water_Temp = ds18b20.getTempCByIndex(0); 
   } 
-  float Humidity = dht.readHumidity();
-  float Amb_Temp = dht.readTemperature();
-  if (isnan(Amb_Temp) || isnan(Humidity) || Water_Temp == 0)   //Check if returns are valid, if they are NaN (not a number) then something went wrong!
-  {
-    Serial.println("Failed to read from AM2301 or DS1820");
-  } 
-  else
-  {
-    Serial.print("Humidity: "); 
-    Serial.print(Humidity);
-    Serial.print(" %\t");
-    Serial.print("Ambient Temperature: "); 
-    Serial.print(Amb_Temp);
-    Serial.print(" C");
-    Serial.print("\t");
-    Serial.print("Water Temperature");
-    Serial.print(Water_Temp);
-    Serial.println(" C");
-    // Scroll through the data and present on the LCD
-    delay(5000);
- }
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || (Water_Temp == 0)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    //return;
+  }
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+
 }
